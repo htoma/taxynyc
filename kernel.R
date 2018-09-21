421945ea4649cc5b7ecdb323e81a7a47f0f47a95
Horia Toma <hotoma@microsoft.com>
2018-09-21 12:03:30 +0200

    Some cleaning and processing only a random sample of January

--- kernel.R
+++ kernel.R
@@ -13,55 +13,58 @@ library(VIM)
 library(lubridate)
 library(leaflet)
 library(plyr)
+library(tibble)
+library(dplyr)
 
-taxi = read.csv("c:/data/nyctaxi/train.csv")
-taxismall = taxi[1:10000,]
+taxis <- as.tibble(fread('c:/data/nyctaxi/train.csv'))
 
-#taxismall = complete.cases(taxismall)
-
-# add aerial distance
-#taxismall$distance <- sqrt((taxismall$pickup_latitude-taxismall$dropoff_latitude)^2 + (taxismall$pickup_longitude-taxismall$dropoff_longitude)^2)
-
-summary(taxismall)
-
-set.seed(20)
-clusterdata <- taxismall[,c(6:7)]
-clusters <- kmeans(clusterdata,6)
-taxismall$cluster <- as.factor(clusters$cluster)
-
-# check if there are missing values
-#aggr(taxismall)
-
-# extract year/month/day
-taxismall$pickupweekday <- wday(taxismall$pickup_datetime)
-
-sbbox <- make_bbox(lon = taxismall$pickup_longitude, lat = taxismall$pickup_latitude, f = .1)
-sq_map <- get_map(location = sbbox, maptype = "terrain", source = "google")
-ggmap(sq_map) + geom_point(data = taxismall, mapping = aes(x = pickup_longitude, y = pickup_latitude), color = as.factor(taxismall$cluster))
-#NYCMap <- get_map("New York")
-
-pal <- colorNumeric(
-  palette = "Blues",
-  domain = as.numeric(taxismall$cluster))
-
-#https://rstudio.github.io/leaflet/colors.html
-leaflet(data = taxismall) %>% addProviderTiles("Esri.NatGeoWorldMap") %>%
-  addCircleMarkers(~pickup_longitude, ~pickup_latitude, radius = 1, 
-                   color = ~pal(as.numeric(taxismall$cluster)), fillOpacity = 0.3)
+# check data 
+summary(taxis)
 
 # reformating featues
-taxismall <- taxismall %>%
-              mutate(pickup_datetime = ymd_hms(pickup_datetime),
-                     dropoff_datetime = ymd_hms(dropoff_datetime),
-                     passenger_count = factor(passenger_count))
+taxis <- taxis %>%
+  mutate(pickup_datetime = ymd_hms(pickup_datetime),
+         dropoff_datetime = ymd_hms(dropoff_datetime),
+         passenger_count = factor(passenger_count))
+
+# filter only january data (and then a random sample of 10k points)
+january <- taxis[month(taxis$pickup_datetime) == 1,]
+january <- sample_n(january, 20000)
+
+# check if there are missing values
+aggr(january)
+
+# k-means clustering
+set.seed(20)
+clusterdata <- january[,c(6:7)]
+clusters <- kmeans(clusterdata,6)
+january$cluster <- as.factor(clusters$cluster)
+
+# leaflef map
+#https://rstudio.github.io/leaflet/colors.html
+pal <- colorNumeric(
+  palette = "Reds",
+  domain = as.numeric(january$cluster))
+
+leaflet(data = january) %>% addProviderTiles("Esri.NatGeoWorldMap") %>%
+  addCircleMarkers(~pickup_longitude, ~pickup_latitude, radius = 1, 
+                   color = ~pal(as.numeric(january$cluster)), fillOpacity = 0.3)
+
+# ggmap way
+sbbox <- make_bbox(lon = january$pickup_longitude, lat = january$pickup_latitude, f = .1)
+sq_map <- get_map(location = sbbox, maptype = "terrain", source = "google")
+ggmap(sq_map) + geom_point(data = january, 
+                           mapping = aes(x = pickup_longitude, y = pickup_latitude), 
+                           color = as.factor(january$cluster))
+
 
 # anomaly in duration
-taxismall %>%
+january %>%
   ggplot(aes(trip_duration)) + 
   geom_histogram(fill = "red", bins = 150) +
   scale_x_log10()
 
 # pickup dates
-taxismall %>%
+january %>%
   ggplot(aes(pickup_datetime)) + 
   geom_histogram(fill = "red", bins = 100)
